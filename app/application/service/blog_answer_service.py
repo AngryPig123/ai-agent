c324c3fa9de77c8de333ba77de371482569237bb
Angrypig123 <shguddnr3@naver.com>
2026-03-23 01:31:13 +0900

    .

--- app/application/service/blog_answer_service.py
+++ app/application/service/blog_answer_service.py
@@ -29,19 +29,28 @@ class BlogAnswerService(UserAnswerUseCase):
             trace_id=str(uuid.uuid4())
         )
 
-        self.search_blog_tool.execute(
+        search_blog_tool_result, search_blog_tool_state = self.search_blog_tool.execute(
             state=state,
             context=context,
         )
 
-        self.summarize_context_tool.execute(
-            state=state,
+        if not search_blog_tool_result.success:
+            raise RuntimeError(search_blog_tool_result.error)
+
+        summarize_context_tool_result,summarize_context_tool_state = self.summarize_context_tool.execute(
+            state=search_blog_tool_state,
             context=context
         )
 
-        self.answer_draft_tool.execute(
-            state=state,
+        if not summarize_context_tool_result.success:
+            raise RuntimeError(summarize_context_tool_result.error)
+
+        answer_draft_tool_result, answer_draft_tool_state = self.answer_draft_tool.execute(
+            state=summarize_context_tool_state,
             context=context
         )
 
-        return state["answer"]
+        if not answer_draft_tool_result.success:
+            raise RuntimeError(answer_draft_tool_result.error)
+
+        return answer_draft_tool_state["answer"]
